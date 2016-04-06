514847570276964497b4a61e5a492de6ca094b82
Richard Munoz <rmunoz@cs.columbia.edu>
2016-04-06 00:27:29 -0400

    Adaboost cross-validation predictions

--- ml2016/adaboost.py
+++ ml2016/adaboost.py
@@ -1,10 +1,10 @@
 import logging
-import time
 
 import numpy as np
-import scipy.io as sio
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.cross_validation import cross_val_score, KFold
 from sklearn.ensemble import AdaBoostClassifier
+from sklearn.metrics import zero_one_loss
 
 
 logging.basicConfig(level=logging.INFO)
@@ -12,10 +12,9 @@ logger = logging.getLogger(__name__)
 
 
 def train_adaboost(X, Y, n_est=100):
-    logger.info("Training adaboost with max iterations: %d" % n_est)
+    logger.info("Training adaboost with n_estimators: %d" % n_est)
 
-    Y = Y.toarray()
-    Y = Y.ravel()
+    Y = Y.toarray().ravel()
 
     # clf = DecisionTreeClassifier(max_depth=1)
     clf = AdaBoostClassifier(n_estimators=n_est)
@@ -27,5 +26,20 @@ def train_adaboost(X, Y, n_est=100):
                 .format(n_est, loss_train, 1 - score_train))
     return clf
 
+
+def train_cv(X, Y, max_n_est=100):
+    logger.info("Training cross-validated adaboost")
+
+    Y = Y.toarray().ravel()
+    cv = KFold(X.shape[0], n_folds=10, shuffle=True, random_state=92309)
+    err_cv = np.zeros((max_n_est,))
+
+    for n_est in range(1, max_n_est + 1):
+        clf = AdaBoostClassifier(n_estimators=n_est)
+        scores = cross_val_score(clf, X, Y, cv=cv)
+        logger.info("n_est: %d \t cv_err: %f" % (n_est, 1 - scores.mean()))
+        err_cv[n_est - 1] = 1 - scores.mean()
+
+    return err_cv
 
 
--- predict_adaboost.py
+++ predict_adaboost.py
@@ -8,8 +8,11 @@ from __future__ import division
 import logging
 from argparse import ArgumentParser
 
+from matplotlib import pyplot as plt
+import numpy as np
+
 from config import config
-from ml2016.adaboost import train_adaboost
+from ml2016.adaboost import train_adaboost, train_cv
 from ml2016.preprocess import load_data, extract_xy
 from ml2016.submit import save_submission
 
@@ -17,14 +20,27 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
+def plot(err_cv):
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.plot(np.arange(len(err_cv)) + 1, err_cv,
+            label='Real AdaBoost CV Error')
+    ax.set_ylim((0.0, 0.5))
+    ax.set_xlabel('n_estimators')
+    ax.set_ylabel('error rate')
+    leg = ax.legend(loc='upper right', fancybox=True)
+    leg.get_frame().set_alpha(0.7)
+    plt.show()
+
+
 def get_args():
     p = ArgumentParser(description="Predict using adaboost")
     p.add_argument("-f", "--filename",
                    help="output filename (default: %(default)s)",
-                   type=str, default='predict-avg.csv')
+                   type=str, default='predict-adaboost.csv')
     p.add_argument('-n', '--n-est',
                    help="max adaboost estimators (default: %(default)s)",
-                   type=int, default=10000)
+                   type=int, default=100)
     args = p.parse_args()
     return args
 
@@ -36,14 +52,27 @@ def main():
                                config.paths.cache_folder)
     Xs, Ys, col_names_S = extract_xy(S, col_names_S)
 
-    clf = train_adaboost(Xs, Ys, args.n_est)
 
-    # T, col_names_T = load_data(config.paths.test_data,
-    #                            config.paths.feat_types,
-    #                            config.paths.cache_folder)
-    logger.info('predicting with probability equal to avg')
-    # pred = predict_on_avg(T, frac_positive)
-    # save_submission(pred, config.paths.out_folder + args.filename)
+    err_cv = train_cv(Xs, Ys, args.n_est)
+    logger.info("minimum cv error: %f" % min(err_cv))
+
+    plot(err_cv)
+
+    logger.info("training cross-validated classifier")
+    clf = train_adaboost(Xs, Ys, list(err_cv).index(min(err_cv)))
+
+    T, col_names_T = load_data(config.paths.test_data,
+                               config.paths.feat_types,
+                               config.paths.cache_folder)
+    # Xt, Yt, col_names_t = extract_xy(T, col_names_T)
+    Xt = T
+
+    logger.info('predicting test set labels')
+    test_pred = clf.predict(Xt)
+    pred = {}
+    for i, lbl in enumerate(test_pred):
+        pred[i + 1] = lbl
+    save_submission(pred, config.paths.out_folder + args.filename)
 
 
 if __name__ == '__main__':
